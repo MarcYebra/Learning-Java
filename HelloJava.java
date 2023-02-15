1575fd78b06ba89f6307cd10d289fd7716e9f809
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2023-02-15 13:27:08 -0600

    Update and rename HelloJava to HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+import javax.swing.*
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Hello World!");
+    JLabel label = new JLabel("Hello, World!", JLabel.CENTER);
+    frame.add(label);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
+  }
+}
+
